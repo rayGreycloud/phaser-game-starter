006fc52f9cb411624301a2616a5896c0ed9a8c33
rayGreycloud <rayGreycloud@gmail.com>
2017-07-11 11:59:54 -0700

    Add stateInit

---
+++ js/states/stateInit.js
@@ -0,0 +1,36 @@
+var StateInit = {
+
+  preload: function () {
+    game.load.image("loadingEmpty", "assets/images/loading/progress_none.png");
+    game.load.image("loadingFull", "assets/images/loading/progress_all.png");
+
+    if (screen.width < 1500) {
+
+      if (useLandscape == true) {
+        game.scale.forceOrientation(true, false);
+      } else {
+        game.scale.forceOrientation(false, true);
+      }
+
+      game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
+      game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+    }
+  },
+
+  create: function () {
+    game.state.start("StateLoad");
+  },
+
+  update: function () {
+
+  },
+
+  rightWay: function () {
+    document.getElementById(wrongTag).style.display = "none";
+  },
+
+  wrongWay: function () {
+    document.getElementById(wrongTag).style.display = "block";
+  }
+
+}

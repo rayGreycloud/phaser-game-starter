8ccf3b9f0d8a4386f502fe37216fc475e3f147aa
rayGreycloud <rayGreycloud@gmail.com>
2017-07-12 06:08:26 -0700

    Refactor high score checker

--- js/utils/saver.js
+++ js/utils/saver.js
@@ -1,14 +1,39 @@
 function checkHighScore(key, score) {
-  if (localStorage.getItem(key) === null) {
+// 2nd refactor
+  var currentHighScore = localStorage.getItem(key);
+
+  if (currentHighScore === null || score > currentHighScore) {
     localStorage.setItem(key, score);
-    highScore = score;
+    return score;
   } else {
-    if (score > localStorage.getItem(key)) {
-      localStorage.setItem(key, score);
-      highScore = score;
-    } else {
-      return localStorage.getItem(key);
-    }
+    return currentHighScore;
   }
-  return highScore;
 }
+
+// 1st refactor
+//   var currentHighScore = localStorage.getItem(key);
+//
+//   if (currentHighScore === null || score > currentHighScore) {
+//     localStorage.setItem(key, score);
+//     highScore = score;
+//   } else {
+//     highScore = currentHighScore;
+//   }
+//
+//   return highScore;
+// }
+
+// Original code
+//   if (localStorage.getItem(key) === null) {
+//     localStorage.setItem(key, score);
+//     highScore = score;
+//   } else {
+//     if (score > localStorage.getItem(key)) {
+//       localStorage.setItem(key, score);
+//       highScore = score;
+//     } else {
+//       return localStorage.getItem(key);
+//     }
+//   }
+//   return highScore;
+// }

b2dc47729228eb86df269343c3ecd6f17770f132
rayGreycloud <rayGreycloud@gmail.com>
2017-07-12 08:36:13 -0700

    Add main game state

---
+++ js/states/stateMain.js
@@ -0,0 +1,51 @@
+var StateMain = {
+  preload: function () {
+
+  },
+
+  create: function () {
+
+    // Reset score
+    score = 0;
+
+    // Add buttons
+    this.btnYes = gameButtons.addButton('yes', -1, -1, this.sayYes, this);
+    this.btnNo = gameButtons.addButton('no', -1, this.btnYes.y - this.btnYes.height, this.sayNo, this);
+
+    this.btnMusic = gameButtons.addAudioButton('music', 20, 20, gameButtons.toggleMusic, this);
+    this.btnSound = gameButtons.addAudioButton('sound', 20, 70, gameButtons.toggleSound, this);
+
+    // For scrolling game - fix position
+    // this.audioGroup = game.add.group();
+    // this.audioGroup.fixedToCamera = true;
+
+    // Define sounds
+    this.elephant = game.add.audio('elephant');
+
+    // Background music
+    // Define
+    this.backgroundMusic = game.add.audio('backgroundMusic');
+    // Add to game object
+    gameMedia.setBackgroundMusic(this.backgroundMusic);
+    // Initialize music
+    gameMedia.updateMusic();
+
+    // Initialize sound buttons
+    gameButtons.updateButtons();
+  },
+
+  sayYes: function () {
+    // Play sound
+    gameMedia.playSound(this.elephant);
+  },
+
+  sayNo: function () {
+    this.backgroundMusic.stop();
+    game.state.start('StateOver');
+  },
+
+  update: function () {
+
+  }
+
+}
